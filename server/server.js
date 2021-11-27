6119e6cd94a013ec8519265b0d1c3f2639d8fdc3
deepakbhattmits <deepakbhattmits@gmail.com>
2021-11-27 22:46:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,12 +26,6 @@ mongoose
     console.log('Error : ', err)
   })
 
-// mongoose.Promise = global.Promise
-// mongoose.connect(MONGO_URI)
-// mongoose.connection
-//   .once('open', () => console.log('Connected to MongoLab instance.'))
-//   .on('error', (error) => console.log('Error connecting to MongoLab:', error))
-
 app.use(bodyParser.json())
 app.use(
   '/graphql',
