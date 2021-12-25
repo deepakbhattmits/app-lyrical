85590f2b51ef98aadea0d3716195defadb725645
deepakbhattmits <deepakbhattmits@gmail.com>
2021-12-25 09:55:20 +0530

    Delete NotFound.jsx

--- client_bk/components/NotFound.jsx
+++
@@ -1,3 +0,0 @@
-import {Link} from 'react-router-dom'
-const NotFound = () => <h4>Page you are looking is not found, <Link to='/'> click here to go back to home</Link></h4>;
-export default NotFound
