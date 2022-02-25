087510156efba091197651a5c082354df90116d0
deepakbhattmits <deepakbhattmits@gmail.com>
2022-02-25 09:49:13 +0530

    Delete LyricListItem.jsx

--- client_/components/LyricListItem.jsx
+++
@@ -1,9 +0,0 @@
-const LyricListItem=({content,likes,id,handleLike}) =>
-    <li className='collection-item likes'>
-        {content}
-        <div className='vote-box'>
-            <i onClick={() => handleLike(id,likes)} className="material-icons right link ">thumb_up</i>
-            {likes}
-        </div>
-    </li>;
-export default LyricListItem
